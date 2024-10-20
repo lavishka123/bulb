abde96396d74429927534a9b61f467d95436e9a1
lavishka123 <lavishkakumari0903@gmail.com>
2024-10-20 22:30:27 +0500

    bulb on/off

---
+++ app.js
@@ -0,0 +1,11 @@
+var bulb = document.getElementById("bulb")
+
+function bulbOn() {
+    document.getElementById("imgoff").disabled=false
+    bulb.src ="images/bulb-on-removebg-preview1.jpeg";
+
+}
+function bulbOff() {
+    bulb.src ="images/bulb-off-removebg-preview.jpeg";
+
+}
